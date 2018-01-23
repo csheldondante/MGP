a75580260df90c0923f8bcbf0b924689e6b40ab6
qemqemqem <richardson.andrew@gmail.com>
2018-01-23 11:03:52 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// This is the Main unity component that sets up the game startup scene and intializes the startup routine
+/// This is the Main unity component that sets up the game startup scene and intializes the startup routine.
 /// </summary>
 public class Main : MonoBehaviour {
 	public GameObject _splashScreen;
